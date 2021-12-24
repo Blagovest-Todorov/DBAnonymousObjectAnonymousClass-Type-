708a0a88e54cb62b48a42a6573f43690a1585517
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-12-24 09:17:35 +0200

    added newFile

---
+++ NewExample/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using SoftUni2.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni2
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //var softUniContext = new SoftUniContext();
+            //var result = GetEmployeesWithSalaryOver50000(softUniContext);
+            //Console.WriteLine(result);
+
+            var softUniContext = new SoftUniContext(); //create an instance of the DB SoftUni
+            var result = GetEmployeesFromRandD(softUniContext);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFromRandD(SoftUniContext context) 
+        {
+            var employeesFromRandD = context.Employees
+                                            .Where(x => x.Department.Name == "Research and Development")
+                                            .Select(x => new
+                                            {
+                                                x.FirstName,
+                                                x.LastName,
+                                                DepartmentName = x.Department.Name,
+                                                x.Salary,
+                                            })
+                                            .OrderBy(x => x.Salary)
+                                            .ThenByDescending(x => x.FirstName)
+                                            .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employeesFromRandD)
+            {
+                sb.AppendLine($"{employee.FirstName} from {employee.DepartmentName} - ${employee.Salary:F2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+
+
+        
+    }
+}
